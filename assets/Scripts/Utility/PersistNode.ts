b8cff6d716f450f77853cfc68ce854bc8186c333
WWW_cool <WWW_cool@inbox.ru>
2018-11-20 23:43:22 +0300

    work in progress

---
+++ assets/Scripts/Utility/PersistNode.ts
@@ -0,0 +1,26 @@
+// Learn TypeScript:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+import {Singleton} from "Utility/Singleton";
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class PersistNode extends Singleton{
+
+    @property
+    isPersist: boolean = true;
+
+    onLoad () {
+		if(!cc.Game.isPersistRootNode(this.node) && this.isPersist){
+			cc.Game.addPersistRootNode(this.node);
+		}
+    }
+}
---
+++ assets/Scripts/Utility/Singleton.ts
@@ -0,0 +1,24 @@
+// Learn TypeScript:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+export class Singleton extends cc.Component{
+	private static instance: Singleton;
+	// want to do private here but compiler kick me off
+	protected constructor() { 
+		super();
+	}
+
+	static getInstance() {
+		if (!Singleton.instance) {
+			Singleton.instance = new Singleton();
+		}
+		return Singleton.instance;
+	}
+}
