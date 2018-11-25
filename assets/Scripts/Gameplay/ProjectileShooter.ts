36013cd91b4850ffac7b157a1af170ae34eafb14
Anton Shalgachev <anton.shalgachev@gmail.com>
2018-11-25 14:39:53 +0300

    minigun overheating

--- assets/Scripts/Gameplay/ProjectileShooter.ts
+++ assets/Scripts/Gameplay/ProjectileShooter.ts
@@ -29,7 +29,17 @@ export default class ProjectileShooter extends cc.Component {
 	@property
 	weaponActive: boolean = true;
 
+	@property
+	heatPerShot: number = 0.0;
+	@property
+	heatDissipationPerSecond: number = 0.0;
+	@property
+	heatThreshold: number = 0.0;
+
 	cooldown: number = 0.0;
+
+	heat: number = 0.0;
+	overheated: boolean = false;
 
 	start () {
 		this.cooldown = this.reloadSpeed;
@@ -38,13 +48,15 @@ export default class ProjectileShooter extends cc.Component {
 	update (dt) {
 		this.cooldown -= dt;
 
-		if (this.cooldown < 0.0 && this.weaponActive) {
+		if (this.cooldown < 0.0 && this.weaponActive && !this.overheated) {
 			this.shoot();
 			this.cooldown = this.reloadSpeed;
 		}
+
+		this.updateHeat(this.heat - this.heatDissipationPerSecond * dt);
 	}
 
-	shoot () {
+	private shoot () {
 		// TODO antonsh use NodePool
 		let bullet = cc.instantiate(this.projectilePrefab);
 		bullet.parent = this.node;
@@ -59,7 +71,17 @@ export default class ProjectileShooter extends cc.Component {
 			vel.addSelf(this.parentBody.linearVelocity);
 		body.linearVelocity = vel;
 
-		// DebugHelper.logHierarchy();
+		this.updateHeat(this.heat + this.heatPerShot);
+	}
+
+	private updateHeat (newHeat: number) {
+		if (newHeat >= 1.0)
+			this.overheated = true;
+
+		this.heat = MathHelper.clamp(newHeat, 0.0, 1.0);
+
+		if (this.heat < this.heatThreshold)
+			this.overheated = false;
 	}
 
 	public getProgress () {
